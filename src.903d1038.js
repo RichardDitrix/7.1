81e96e2f12f2c9469ae9c7d545ca95a5dc5706e3
RichardDitrix <89538231+RichardDitrix@users.noreply.github.com>
2021-12-03 08:43:30 +0000

    Deploying to gh-pages from @ RichardDitrix/goit-project-team7@35a6f08878cf2f2566f58c80c0e4be0beabb61b2 🚀

--- src.903d1038.js
+++ src.903d1038.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-project-team7/src.1573ce11.js.map
+//# sourceMappingURL=/goit-project-team7/src.903d1038.js.map
